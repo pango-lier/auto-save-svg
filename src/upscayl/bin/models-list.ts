c1d75d0db1d0ce1b631f0ab64cd04a8741377650
Binh Trong <binhtrongcdt1@gmail.com>
2024-08-25 16:37:20 +0700

    add model upscayl

--- src/upscayl/bin/models-list.ts
+++ src/upscayl/bin/models-list.ts
@@ -1,17 +1,36 @@
-export enum EModelList {
-  RealesrganX4plus = 'realesrgan-x4plus',
-  RealesrganX4fast = 'realesrgan-x4fast',
+export enum UpscaylModels {
+  GeneralPhotoRealEsrganX4plus = 'realesrgan-x4plus',
+  GeneralPhotoRealesrganX4fast = 'realesrgan-x4fast',
+  GeneralPhotoRemacri = 'remacri',
+  GeneralPhotoUltramixBalanced = 'ultramix_balanced',
+  GeneralPhotoUltrasharp = 'ultrasharp',
+  DigitalArtRealesrganX4plusAnime = 'realesrgan-x4plus-anime',
 }
 export const defaultModelsList = [
-  { label: 'General Photo (Real-ESRGAN)', value: EModelList.RealesrganX4plus },
+  {
+    label: 'General Photo (Real-ESRGAN)',
+    value: UpscaylModels.GeneralPhotoRealEsrganX4plus,
+  },
   {
     label: 'General Photo (Fast Real-ESRGAN)',
-    value: EModelList.RealesrganX4fast,
+    value: UpscaylModels.GeneralPhotoRealesrganX4fast,
   },
-  { label: 'General Photo (Remacri)', value: 'remacri' },
-  { label: 'General Photo (Ultramix Balanced)', value: 'ultramix_balanced' },
-  { label: 'General Photo (Ultrasharp)', value: 'ultrasharp' },
-  { label: 'Digital Art', value: 'realesrgan-x4plus-anime' },
+  {
+    label: 'General Photo (Remacri)',
+    value: UpscaylModels.GeneralPhotoRemacri,
+  },
+  {
+    label: 'General Photo (Ultramix Balanced)',
+    value: UpscaylModels.GeneralPhotoUltramixBalanced,
+  },
+  {
+    label: 'General Photo (Ultrasharp)',
+    value: UpscaylModels.GeneralPhotoUltrasharp,
+  },
+  {
+    label: 'Digital Art',
+    value: UpscaylModels.DigitalArtRealesrganX4plusAnime,
+  },
 ];
 
 export const DEFAULT_MODELS = defaultModelsList.map((model) => model.value);
